72d1785cda3680c580de52e397524fa09933f7ad
Murilo Alves Pereira Varoto <66957888+Muriloapv@users.noreply.github.com>
2023-08-20 10:47:41 -0300

    Add files via upload

---
+++ pessoa.java
@@ -0,0 +1,6 @@
+//Crie uma classe Java (versão 17) Pessoa com os atributos Nome, Data de Nascimento e Idade.
+//Crie no mínimo três Construtores e métodos (getters e setters).
+
+public class pessoa {
+
+}
