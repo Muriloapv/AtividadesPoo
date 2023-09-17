2a8661398be360c027758bd04d00c533b93b6668
Murilo Alves Pereira Varoto <66957888+Muriloapv@users.noreply.github.com>
2023-09-17 16:11:28 -0300

    Update pessoa.java

--- pessoa.java
+++ pessoa.java
@@ -1,6 +1,25 @@
 //Crie uma classe Java (versão 17) Pessoa com os atributos Nome, Data de Nascimento e Idade.
 //Crie no mínimo três Construtores e métodos (getters e setters).
 
-public class pessoa {
+package org.example.domain;
 
+import org.example.domain.enums.EstadoCivil;
+
+public abstract class Pessoa {
+
+    private String nome;
+    private EstadoCivil estadoCivil;
+
+    public Pessoa(String nome, EstadoCivil estadoCivil) {
+        this.nome = nome;
+        this.estadoCivil = estadoCivil;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public EstadoCivil getEstadoCivil() {
+        return estadoCivil;
+    }
 }
